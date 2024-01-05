33d897cf303e9f8583a9e6f37941447fd2c9ca8e
Agustin Dev <agustinfruni@gmail.com>
2024-01-05 02:14:04 -0300

    experience fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://AgustinFRUni.github.io'
+  integrations: [tailwind()],
+  site: 'https://AgustinFRUni.github.io',
 });
